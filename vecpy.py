4e20625d13ee156dc0f286353283d98bc6260e4c
AzuRakaH <69580109+dct-LuLu@users.noreply.github.com>
2023-03-13 08:14:18 +0100

    Update vecpy.py

--- vecpy.py
+++ vecpy.py
@@ -9,7 +9,6 @@ game_window = pyglet.window.Window(800, 600)
 
 main_batch = pyglet.graphics.Batch()
 
-pyglet.window.FPSDisplay.update_period = 0.01
 counter = pyglet.window.FPSDisplay(window=game_window)
 
 label = pyglet.text.Label("", color=(122, 122, 122, 255),
@@ -18,16 +17,16 @@ label = pyglet.text.Label("", color=(122, 122, 122, 255),
                           anchor_x='center', anchor_y='center')
 
 
-class PhysicsObject:
-    def __init__(self, obj: shapes):
-        self.obj = obj
+class PhysicalObject(pyglet.shapes.Circle):
+    def __init__(self, *args, **kwargs):
+        super(PhysicalObject, self).__init__(*args, **kwargs)
         self.speed_x = 0
         self.speed_y = 0
         self.gravity = 0.05
         self.speed_gravity = 0
 
 
-circle = PhysicsObject(shapes.Circle(x=100, y=150, radius=100, color=(50, 225, 30)))
+circle = PhysicalObject(x=100, y=150, radius=100, color=(50, 225, 30))
 
 midcircle = shapes.Circle(x=100, y=150, radius=2, color=(245, 40, 145, 120))
 
@@ -46,7 +45,7 @@ def reset_plane():
 def on_mouse_motion(x, y, dx, dy):
     label.text = f'x:{x} y:{y}'
 
-    if circle.obj.radius > math.sqrt((x-circle.obj.x)**2+(y-circle.obj.y)**2):
+    if circle.radius > math.sqrt((x-circle.x)**2+(y-circle.y)**2):
         label.color = (255, 0, 255, 255)
     else:
         label.color = (122, 122, 122, 255)
@@ -56,9 +55,9 @@ def on_mouse_motion(x, y, dx, dy):
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
     global inn
     if buttons & mouse.LEFT:
-        if circle.obj.radius > math.sqrt((x-circle.obj.x)**2+(y-circle.obj.y)**2) or inn:
-            circle.obj.x += dx
-            circle.obj.y += dy
+        if circle.radius > math.sqrt((x-circle.x)**2+(y-circle.y)**2) or inn:
+            circle.x += dx
+            circle.y += dy
             circle.speed_gravity = 0
             circle.speed_x = dx
             circle.speed_y = dy
@@ -81,11 +80,11 @@ def on_draw():
     label.draw()
     if not inn:
         circle.speed_gravity += circle.gravity
-        circle.obj.x += circle.speed_x
-        circle.obj.y -= circle.speed_y + circle.speed_gravity
+        circle.x += circle.speed_x
+        circle.y -= circle.speed_y + circle.speed_gravity
 
-    circle.obj.draw()
-    midcircle.x, midcircle.y = circle.obj.x, circle.obj.y
+    circle.draw()
+    midcircle.x, midcircle.y = circle.x, circle.y
     midcircle.draw()
 
 
